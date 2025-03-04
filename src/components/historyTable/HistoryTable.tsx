621afe5553817d6b94a0c2b7f31ec90bee9d2b47
NeoEx <draja.acc@gmail.com>
2025-03-04 10:48:03 -0300

    feat: done history component

--- src/components/historyTable/HistoryTable.tsx
+++ src/components/historyTable/HistoryTable.tsx
@@ -1,73 +1,62 @@
 import { useSelector } from "react-redux";
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
-import { useState, useRef, useEffect } from 'react';
-
-const people = [
-  { id: 1, name: 'Tom Cook' },
-  { id: 2, name: 'Wade Cooper' },
-  { id: 3, name: 'Tanya Fox' },
-  { id: 4, name: 'Arlene Mccoy' },
-  { id: 5, name: 'Devon Webb' },
-];
+import { useState, useEffect } from 'react';
+import Table from '../table/Table';
 
 const HistoryTable = () => {
   const cepList = useSelector((state: any) => state.cep.list);
-  const [selected, setSelected] = useState(people[1]);
-  const [firstOpen, setFirstOpen] = useState(true);
-
-  const listboxRef = useRef<HTMLDivElement>(null);
-
-  const scrollToListbox = () => {
-    if (listboxRef.current) {
-      const rect = listboxRef.current.getBoundingClientRect();
-      const scrollY = window.scrollY + rect.top - (window.innerHeight / 4); // Opcional: ajusta 1/4 da tela
-      window.scrollTo({
-        top: scrollY,
-        behavior: 'smooth',
-      });
-    }
-  };
+  const [selected, setSelected] = useState<any>(null);
 
   useEffect(() => {
-    if (firstOpen) {
-      scrollToListbox();
-      setFirstOpen(false);
+    if (cepList.length > 0 && !selected) {
+      setSelected(cepList[0]);
     }
-  }, [firstOpen]);
+  }, [cepList, selected]);
+
+  if (cepList.length === 0) {
+    return (
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-2xl font-semibold tracking-tight text-primary sm:text-5xl">Salve suas pesquisas para poder consultar novamente abaixo.</h2>
+      </div>
+    );
+  }
 
   return (
-    <div className="px-6 sm:py-32 lg:px-8 relative min-h-[30rem]">
+    <div className="px-6 sm:py-32 lg:px-8 relative">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-balance text-3xl font-semibold tracking-tight text-primary sm:text-5xl">
           Veja seu Histórico Salvo de Pesquisas:
         </h2>
 
-        <div className="relative mt-4" ref={listboxRef}>
+        <div className="relative mt-4">
           <Listbox value={selected} onChange={setSelected}>
-            {({ open }) => (
-              <>
-                <ListboxButton className="w-full rounded-lg bg-secondary py-1.5 pr-8 pl-3 text-left text-sm text-primary font-bold">
-                  {selected.name}
-                  <ChevronDownIcon className="absolute top-0 right-2.5 text-primary size-8" />
-                </ListboxButton>
+            <div className="relative">
+              <ListboxButton className="w-full rounded-lg bg-secondary py-1.5 pr-8 pl-3 text-left text-sm text-primary font-bold">
+                {`CEP: ${selected?.cep}`}
+                <ChevronDownIcon className="absolute top-0 right-2.5 text-primary size-8" />
+              </ListboxButton>
 
-                {open && (
-                  <ListboxOptions className="mt-1 w-full top-full rounded-lg border bg-white shadow-lg max-h-60 overflow-auto">
-                    {people.map((person) => (
-                      <ListboxOption
-                        key={person.name}
-                        value={person}
-                        className="cursor-pointer select-none py-2 pl-3 pr-4 text-sm text-gray-900 hover:bg-gray-100"
-                      >
-                        {person.name}
-                      </ListboxOption>
-                    ))}
-                  </ListboxOptions>
-                )}
-              </>
-            )}
+              <ListboxOptions className="mt-1 w-full top-full rounded-lg border bg-primary shadow-lg max-h-60 overflow-auto absolute">
+                {cepList.map((cep: any) => (
+                  <ListboxOption
+                    key={cep.cep}
+                    value={cep}
+                    className="cursor-pointer select-none py-2 pl-3 pr-4 text-sm text-white hover:bg-gray-100 border"
+                  >
+                    {`CEP: ${cep.cep}`}
+                  </ListboxOption>
+                ))}
+              </ListboxOptions>
+            </div>
           </Listbox>
+
+          {/* Exibe tabela com dados do CEP selecionado */}
+          {selected && (
+            <div className="mt-4">
+              <Table data={selected} />
+            </div>
+          )}
         </div>
       </div>
     </div>
